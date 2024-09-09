6577f26921398d2bb7f7efdbc0f240ff056dc116
Julian Florez <forezjuli08@hotmail.com>
2024-09-09 18:07:25 -0500

    update - healthcheck handler and test to send a json response

--- cmd/api/healthcheck_test.go
+++ cmd/api/healthcheck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -9,16 +10,21 @@ import (
 
 func TestHealthCheck(t *testing.T) {
 
-	expectedBody := "status: available\nenvironment: development\nversion: 1.0.0"
+	// expectedBody := "status: available\nenvironment: development\nversion: 1.0.0"
+	expectedBody := `{"status": "available", "environment": "development", "version": "1.0.0"}`
+	expectedHeader := "application/json"
 
 	app := NewTestApplication(t)
 
 	ts := NewTestServer(t, app.routes())
 	defer ts.Close()
 
-	status, _, body := ts.get(t, "/v1/healthcheck")
+	status, header, body := ts.get(t, "/v1/healthcheck")
+	fmt.Printf("header map: %+v\n", header)
+	fmt.Printf("content type for healthcheck test: %q\n", header.Get("Content-Type"))
 
 	assert.Equal(t, status, http.StatusOK)
+	assert.Equal(t, header.Get("Content-Type"), expectedHeader)
 	assert.Equal(t, body, expectedBody)
 
 }
