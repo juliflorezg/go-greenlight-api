2dcdc45e23bc0a1a8ea3b2a5c743773a8aa79605
Julian Florez <forezjuli08@hotmail.com>
2024-09-09 17:33:55 -0500

    add - new routes for GET movies and POST a new movie

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,7 +5,9 @@ import "net/http"
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
+	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 
 	return mux
 }
