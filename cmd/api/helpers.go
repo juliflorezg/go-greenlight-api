0e85732fd9f3056c2e9e3f04642f3f16944dfa12
Julian Florez <forezjuli08@hotmail.com>
2024-09-09 19:24:38 -0500

    add - writeJSON helper to send JSON responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
 )
 
+// Retrieve the "id" URL parameter from the current request, then convert it to
+// an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 
 	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
@@ -15,4 +18,28 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 
 	return id, nil
 
+}
+
+//	Define a writeJSON() helper for sending responses. This takes the destination
+//
+// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
+// header map containing any additional HTTP headers we want to include in the response.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Append a newline to make it easier to view in terminal applications (remove for testing)
+	js = append(js, '\n')
+
+	for k, v := range headers {
+		w.Header()[k] = v
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
