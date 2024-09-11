6c846067a16c8eaa60005dce3912e5f9ba73af0b
Julian Florez <forezjuli08@hotmail.com>
2024-09-11 09:08:40 -0500

    update - get id from request context

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,13 +5,16 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // Retrieve the "id" URL parameter from the current request, then convert it to
 // an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
 
-	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
