07149b3c3ea018bd6f2f5e49ff6df62c83a49634
Julian Florez <forezjuli08@hotmail.com>
2024-09-11 09:08:24 -0500

    update - use of third party router

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,13 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
-	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
-	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler) //GET by default
 
-	return mux
+	return router
 }
