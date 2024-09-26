feb9841f561ae4c44a8c9d57685359a09b7c8000
Julian Florez <forezjuli08@hotmail.com>
2024-09-26 08:40:29 -0500

    add - simple CORS middleware to allow requests from all origins (dev)

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -19,3 +19,12 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// todo: restrict when app gets to prod
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
