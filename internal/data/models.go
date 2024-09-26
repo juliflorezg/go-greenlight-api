f773631deaf0385a524a28e89bc36dc82789a17e
Julian Florez <forezjuli08@hotmail.com>
2024-09-26 09:25:14 -0500

    add - manage edit conflict with optimistic locking on the DB

--- internal/data/models.go
+++ internal/data/models.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("unable to update resource due to edit conflict")
 )
 
 type MoviesInterface interface {
