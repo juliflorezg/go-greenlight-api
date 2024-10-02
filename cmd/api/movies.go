774235ff78c091dfd6f18b41c5ae7d38142c9d35
Julian Florez <forezjuli08@hotmail.com>
2024-10-02 10:47:54 -0500

    update - use GetAll DB model method in handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -212,6 +212,18 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, 200, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+	// fmt.Fprintf(w, "%+v\n", input)
 
 }
