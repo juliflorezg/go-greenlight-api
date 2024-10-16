4f6fb97a2f5f5084b53c1835a7ec1604ff64c938
Julian Florez <forezjuli08@hotmail.com>
2024-10-16 16:29:38 -0500

    add - filter functions for retrieving sort and sortDirection values to pass them to SQL query

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/juliflorezg/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/juliflorezg/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -16,4 +20,25 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be less than or equal to 100")
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "must be a valid sort value")
+}
+
+// Check that the client-provided Sort field matches one of the entries in our safe-list
+// and if it does, extract the column name from the Sort field by stripping the leading
+// hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
+// Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
